11b79a8547bc73414057ebb96561688946d288e0
Josh Wegener <JoshWegener@gmail.com>
2016-09-22 16:05:35 -0500

    added stubs for the AdapterInterface

--- src/OpenDriveAdapter.php
+++ src/OpenDriveAdapter.php
@@ -3,10 +3,172 @@
 namespace JoshWegener\FlysystemOpenDrive;
 
 use League\Flysystem\Adapter\Polyfill\NotSupportingVisibilityTrait;
+use League\Flysystem\Adapter\Polyfill\StreamedTrait;
 use League\Flysystem\AdapterInterface;
 use League\Flysystem\Config;
 
 class OneDriveAdapter implements AdapterInterface
 {
     use NotSupportingVisibilityTrait;
+    use StreamedTrait;
+
+    /**
+     * Rename a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function rename($path, $newpath)
+    {
+        // TODO: Implement rename() method.
+    }
+
+    /**
+     * Copy a file.
+     *
+     * @param string $path
+     * @param string $newpath
+     *
+     * @return bool
+     */
+    public function copy($path, $newpath)
+    {
+        // TODO: Implement copy() method.
+    }
+
+    /**
+     * Delete a file.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function delete($path)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    /**
+     * Delete a directory.
+     *
+     * @param string $dirname
+     *
+     * @return bool
+     */
+    public function deleteDir($dirname)
+    {
+        // TODO: Implement deleteDir() method.
+    }
+
+    /**
+     * Create a directory.
+     *
+     * @param string $dirname directory name
+     * @param Config $config
+     *
+     * @return array|false
+     */
+    public function createDir($dirname, Config $config)
+    {
+        // TODO: Implement createDir() method.
+    }
+
+    /**
+     * Check whether a file exists.
+     *
+     * @param string $path
+     *
+     * @return array|bool|null
+     */
+    public function has($path)
+    {
+        // TODO: Implement has() method.
+    }
+
+    /**
+     * List contents of a directory.
+     *
+     * @param string $directory
+     * @param bool $recursive
+     *
+     * @return array
+     */
+    public function listContents($directory = '', $recursive = false)
+    {
+        // TODO: Implement listContents() method.
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMetadata($path)
+    {
+        // TODO: Implement getMetadata() method.
+    }
+
+    /**
+     * Get all the meta data of a file or directory.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getSize($path)
+    {
+        // TODO: Implement getSize() method.
+    }
+
+    /**
+     * Get the mimetype of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getMimetype($path)
+    {
+        // TODO: Implement getMimetype() method.
+    }
+
+    /**
+     * Get the timestamp of a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     */
+    public function getTimestamp($path)
+    {
+        // TODO: Implement getTimestamp() method.
+    }
+
+    /**
+     * Reads a file.
+     *
+     * @param string $path
+     *
+     * @return array|false
+     *
+     * @see League\Flysystem\ReadInterface::read()
+     */
+    public function read($path)
+    {
+        // TODO: Implement read() method.
+    }
+
+    public function write($pash, $contents, Config $config)
+    {
+        // TODO: Implement write() method.
+    }
+
+    public function update($pash, $contents, Config $config)
+    {
+        // TODO: Implement update() method.
+    }
 }
